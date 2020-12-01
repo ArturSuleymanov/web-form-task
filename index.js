fb4a3cb0658f713155ff25924a50f9d23a7f15e7
b-jok <artur.e.suleymanov@gmail.com>
2020-12-01 19:46:28 +0300

    move html and css files to source directory

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-function timestamp() { 
+var express = require('express');
+var app = express();
+var path = require('path');
+app.use(express.static(path.join(process.cwd(), 'source')));
+console.log(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000);
+
+/* function timestamp() { 
     var response = document.getElementById("g-recaptcha-response"); 
     
     if (response == null || response.value.trim() == "") {
@@ -6,4 +13,4 @@ function timestamp() {
         elems["ts"] = JSON.stringify(new Date().getTime());
         document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
        } 
-   } setInterval(timestamp, 500); 
+   } setInterval(timestamp, 500);  */
