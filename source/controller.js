f5fc7716fa59aac62da448877f00664cd379e8d7
b-jok <artur.e.suleymanov@gmail.com>
2020-12-02 19:13:27 +0300

    update index.html, controller.js

--- source/controller.js
+++ source/controller.js
@@ -1,21 +1,57 @@
 function timestamp() { 
-    var response = document.getElementById("g-recaptcha-response"); 
-    
-    if (response == null || response.value.trim() == "") {
-        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
-        elems["ts"] = JSON.stringify(new Date().getTime());
-        document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
-       } 
-   } setInterval(timestamp, 500); 
+  var response = document.getElementById("g-recaptcha-response"); 
 
-document.getElementById("web-form").addEventListener("submit",function(evt) {
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
+  } 
+}
+
+setInterval(timestamp, 500); 
+
+function checkRecaptcha() {
+  document.getElementById("qwerty").addEventListener("submit", function(evt) {
    
-   var response = grecaptcha.getResponse();
-   if(response.length == 0) { 
-     //reCaptcha not verified
-     alert("Please, complete captcha validation!"); 
-     evt.preventDefault();
-     return false;
-   }
-   
- });
+    var response = grecaptcha.getResponse();
+    
+    if(response.length == 0) {
+      alert("Please, complete captcha validation!"); 
+      evt.preventDefault();
+      return false;
+    }
+     
+  });
+}
+
+setTimeout(checkRecaptcha, 0);
+
+function checkvalidity(elem) {
+  if (elem.id === '00N5g000000iJhE') {
+    if (elem.value.trim()) {
+      if (!isValidDate(elem.value)) {
+        elem.classList.add('has-error');
+      } else {
+        elem.classList.remove('has-error');
+      }
+    } else {
+      elem.classList.remove('has-error');
+    }
+  } else if (!elem.value.trim()) {
+    elem.classList.add('has-error');
+  } else if (elem.id === 'email' && !isValidEmail(elem.value)) {
+    elem.classList.add('has-error');
+  } else {
+    elem.classList.remove('has-error');
+  }
+}
+
+function isValidEmail(email) {
+  return /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/.test(email);
+}
+
+function isValidDate(date) {
+  var a = String(new Date().getFullYear).slice(-1);
+  console.log(a);
+  return /(0[1-9]|1[012])\/(0[1-9]|1[0-9]|2[0-9]|3[01])\/(19\d\d|200/ + a + /)/.test(date);
+}
