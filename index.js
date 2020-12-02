963b39ecf78f8044c6d9f29075cd73833fa1f074
b-jok <artur.e.suleymanov@gmail.com>
2020-12-02 14:40:15 +0300

    add home.js

--- index.js
+++ index.js
@@ -4,13 +4,3 @@ var path = require('path');
 app.use(express.static(path.join(process.cwd(), 'source')));
 console.log(process.env.PORT || 5000);
 app.listen(process.env.PORT || 5000);
-
-/* function timestamp() { 
-    var response = document.getElementById("g-recaptcha-response"); 
-    
-    if (response == null || response.value.trim() == "") {
-        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
-        elems["ts"] = JSON.stringify(new Date().getTime());
-        document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
-       } 
-   } setInterval(timestamp, 500);  */
